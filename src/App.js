beaee86c830fcc2b68264b0bd1c9e49abd7716d3
Shreyanshi Verma <shreyanshi.verma200302@gmail.com>
2022-07-14 00:42:33 +0530

    edit react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <Switch>
 
-     hi
-    </div>
+    <Route exact path="/">
+This is home page
+     
+    </Route>
+
+    <Route exact path="/starred">
+This is starred
+    </Route>
+     
+<Route>
+  This is 404 page
+</Route>
+    </Switch>
   );
 }
 
